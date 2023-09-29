44c8403ecb375445dbe37384003b905a2acb1d01
sudoh <sudoh@renmoney.com>
2023-09-29 22:32:57 +0100

    updates

--- src/main/java/Bardness.java
+++ src/main/java/Bardness.java
@@ -3,5 +3,6 @@ public class Bardness {
     public static void main(String[] args) {
 
         System.out.println("This system is freakingly bard");
+        System.out.println("I am Vybe");
     }
 }
